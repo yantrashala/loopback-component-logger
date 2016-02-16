8c5e8c0faccb1d522fb5f6b6243f94944c28834c
pbhadauria2000 <pbhadauria@hotmail.com>
2016-02-16 01:33:07 -0800

    Pushing remaining changes

--- test/loggerTest/server/server.js
+++ test/loggerTest/server/server.js
@@ -1,6 +1,6 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
+var logger = require('../../../index')();
 var app = module.exports = loopback();
 
 app.start = function() {
@@ -8,10 +8,10 @@ app.start = function() {
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+    logger.info('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      logger.info('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
 };
