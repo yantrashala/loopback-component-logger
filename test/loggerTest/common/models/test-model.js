10f7d8fef5bfcdeefff5007b38e2f5a1012635c4
pbhadauria2000 <pbhadauria@hotmail.com>
2016-03-04 01:54:03 -0800

    Adding connector hooks to perfLogger and configured a simple REST connector

--- test/loggerTest/common/models/test-model.js
+++ test/loggerTest/common/models/test-model.js
@@ -3,4 +3,29 @@ var logger = require('../../../../index')('TestModel');
 module.exports = function(TestModel) {
 
     logger.debug('Initializing TestModel');
+
+    TestModel.findCustom = function(cb) {
+        var childModel = TestModel.app.models.childModel;
+        //console.log('result',childModel.name);
+        //cb(null,{});
+        childModel.find(function(err,result){
+            console.log('result',result);
+            cb(err,result);
+        });
+    };
+
+    TestModel.remoteMethod('findCustom', {
+    http: {
+      path: '/findCustom',
+      verb: 'get'
+    },
+    returns: {
+      arg: 'testModel',
+      type: 'testModel',
+      root: true
+    },
+    description: 'Get Account List By Customer Id',
+    notes: 'Returns Account List for a given id'
+  });
+
 };
